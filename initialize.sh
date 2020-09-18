52fe3a701527fc5149e9b4fad1b2dcab060f430b
harshjoshi <harshjoshi211@gmail.com>
2020-09-18 15:37:58 -0400

    Create shared volume during the intialization of the services in the initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Initialization started!"
-echo "(It takes longer if it is a very first run. Please wait for few minutes!)"
 mkdir -p log
 touch log/initialize.log && \
 
 echo -ne "Creating Docker volumes ..."
 docker volume create mysql &>> log/initialize.log && \
 docker volume create airflow &>> log/initialize.log && \
+docker volume create shared-volume &>> log/initialize.log && \
 echo  -e "\\rCreating Docker volumes ... \e[32mdone\e[0m" && \
 echo -ne "\\rInitializing sequential executor ..."
 docker-compose -f docker-compose-initialize.yml up -d &>> log/initialize.log
