83bac25e18d1e5a88ce213ac3a19e562f31f55d0
harshjoshi <harshjoshi211@gmail.com>
2020-09-30 16:29:34 -0400

    Create directory "/dataeng" in the hdfs during intialization of the hadoop service Create directory /ontario-school-data in the shared_volume during initialization of services Export DOCKER_CLIENT_TIMEOUT and COMPOSE_HTTP_TIMEOUT variables

--- initialize.sh
+++ initialize.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 echo "Initialization started!"
-mkdir -p log
-touch log/initialize.log && \
+
+echo -ne "Creating files and variables ..."
+mkdir -p log &>> log/initialize.log && \
+touch log/initialize.log &>> log/initialize.log && \
+export DOCKER_CLIENT_TIMEOUT=120 &>> log/initialize.log && \
+export COMPOSE_HTTP_TIMEOUT=120 &>> log/initialize.log && \
+echo  -e "\\rCreating files and variables ... \e[32mdone\e[0m" || \
+echo  -e "\\rCreating files and variables ... \e[31merror\e[0m"
 
 echo -ne "Creating Docker volumes ..."
 docker volume create mysql &>> log/initialize.log && \
 docker volume create airflow &>> log/initialize.log && \
-docker volume create shared-volume &>> log/initialize.log && \
+docker volume create shared_volume &>> log/initialize.log && \
 docker volume create hadoop_namenode &>> log/initialize.log && \
 docker volume create hadoop_datanode &>> log/initialize.log && \
 docker volume create hadoop_historyserver &>> log/initialize.log && \
-echo  -e "\\rCreating Docker volumes ... \e[32mdone\e[0m"
+echo  -e "\\rCreating Docker volumes ... \e[32mdone\e[0m" || \
+echo  -e "\\rCreating Docker volumes ... \e[31merror\e[0m"
 
 echo -ne "Creating Network ..."
 docker network create --attachable --driver overlay dataeng &>> log/initialize.log && \
@@ -21,11 +28,12 @@ echo -ne "\\rInitializing sequential executor ..."
 docker-compose -f docker-compose-initialize.yml up -d &>> log/initialize.log || \
 echo -e "\\rInitializing sequential executor ... \e[31merror\e[0m"
 
-for i in {1..30}
+for i in {1..50}
 do
     if [ "$(docker ps -aq -f status=exited -f name=airflow-init)" ]; then
-        docker-compose -f docker-compose-initialize.yml down &>> log/initialize.log
-        echo -e "\\rInitializing sequential executor ... \e[32mdone\e[0m"
+        docker-compose -f docker-compose-initialize.yml down &>> log/initialize.log && \
+        echo -e "\\rInitializing sequential executor ... \e[32mdone\e[0m" || \
+        echo -e "\\rInitializing sequential executor ... \e[31merror\e[0m"
         break
     fi
     sleep 10s
@@ -44,12 +52,36 @@ echo -e "\\rInitializing local executor ... \e[31merror\e[0m"
 for i in {1..50}
 do    
     if [ "$(docker ps -aq -f status=exited -f name=airflow-init)" ]; then
-        docker-compose -f docker-compose-initialize.yml down &>> log/initialize.log
-        echo -e "\\rInitializing local executor ... \e[32mdone\e[0m"
+        docker-compose -f docker-compose-initialize.yml down &>> log/initialize.log && \
+        echo -e "\\rInitializing local executor ... \e[32mdone\e[0m" || \
+        echo -e "\\rInitializing local executor ... \e[31merror\e[0m"
         break
     fi
     sleep 10s
 done
+
+echo -ne "\\rInitializing hadoop ..."
+docker-compose -f docker-compose-initialize-hadoop.yml up -d &>> log/initialize.log || \
+echo -e "\\rInitializing hadoop ... \e[31merror\e[0m"
+
+for i in {1..50}
+do    
+    if [ "$(docker ps -aq -f status=running -f name=namenode)" ]; then
+        docker container exec -it namenode hdfs dfs -mkdir /dataeng &>> log/initialize.log && \
+        wait
+        docker-compose -f docker-compose-initialize-hadoop.yml down &>> log/initialize.log && \
+        echo -e "\\rInitializing hadoop ... \e[32mdone\e[0m" || \
+        echo -e "\\rInitializing hadoop ... \e[31merror\e[0m"
+        break
+    fi
+    sleep 10s
+done
+
+echo -n "Creating directory in shared volume ..."
+docker container run --rm -v shared_folder:/shared_folder ubuntuvim mkdir -p /shared_folder/ontario-school-data &>> log/initialize.log && \
+docker container run --rm -v shared_folder:/shared_folder ubuntuvim chmod +666 /shared_folder/ontario-school-data &>> log/initialize.log && \
+echo -e "\\rCreating directory in shared volume ... \e[32mdone\e[0m" || \
+echo -e "\\rCreating directory in shared volume ... \e[31merror\e[0m"
 
 echo "Initialization complete!"
 
