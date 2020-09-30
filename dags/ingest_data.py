6c305526a6d874f4a6e2a848ab76eb692982df8a
harshjoshi <harshjoshi211@gmail.com>
2020-09-30 16:48:03 -0400

    Add code in the "ingest_data" to upload file to hdfs

--- dags/ingest_data.py
+++ dags/ingest_data.py
@@ -2,6 +2,8 @@ import datetime as dt
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from upload_to_hdfs import upload_to_hdfs
 
 default_args = {
     'owner' : 'airflow',
@@ -16,7 +18,7 @@ private_school_contact_info_dlink = "https://data.ontario.ca/dataset/7a049187-cf
 academic_year_dlink = "https://files.ontario.ca/opendata/enrolment_by_school_1718_en_supp.xlsx"
 school_info_student_demographics_dlink = "https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/602a5186-67f5-4faf-94f3-7c61ffc4719a/download/new_sif_data_table_2018_2019prelim_en_august.xlsx"
 
-with DAG('ingest_private_school_contact',
+with DAG('ingest_data',
         default_args=default_args,
         schedule_interval='@daily',
         description='A simple dag to download open data files from ontario.ca ',
@@ -29,9 +31,15 @@ with DAG('ingest_private_school_contact',
         download_academic_year = BashOperator(task_id='download_academic_year', bash_command='wget -O /shared-volume/ontario-school-data/'+ academic_year_dlink.split("/")[-1] + ' ' + academic_year_dlink)
         download_school_info_student_demographics = BashOperator(task_id='download_school_info_student_demographics', bash_command='wget -O /shared-volume/ontario-school-data/'+ school_info_student_demographics_dlink.split("/")[-1] + ' ' + school_info_student_demographics_dlink)
         
+        # Upload files to HDFS
+        upload_school_board_school_authority_contact = PythonOperator(task_id='upload_school_board_school_authority_contact', python_callable=upload_to_hdfs, op_kwargs={'fileName': school_board_school_authority_contact_dlink.split("/")[-1]}, dag=dag)
+        upload_public_school_contact_info = PythonOperator(task_id='upload_public_school_contact_info', python_callable=upload_to_hdfs, op_kwargs={'fileName': public_school_contact_info_dllink.split("/")[-1]}, dag=dag)
+        upload_private_school_contact_info = PythonOperator(task_id='upload_private_school_contact_info', python_callable=upload_to_hdfs, op_kwargs={'fileName': private_school_contact_info_dlink.split("/")[-1]}, dag=dag)
+        upload_academic_year = PythonOperator(task_id='upload_academic_year_dlink', python_callable=upload_to_hdfs, op_kwargs={'fileName': academic_year_dlink.split("/")[-1]}, dag=dag)
+        upload_school_info_student_demographics = PythonOperator(task_id='upload_school_info_student_demographics', python_callable=upload_to_hdfs, op_kwargs={'fileName': school_info_student_demographics_dlink.split("/")[-1]}, dag=dag)
 
-        download_school_board_school_authority_contact
-        download_public_school_contact_info
-        download_private_school_contact_info 
-        download_academic_year
-        download_school_info_student_demographics
+        download_school_board_school_authority_contact >> upload_school_board_school_authority_contact
+        download_public_school_contact_info >> upload_public_school_contact_info
+        download_private_school_contact_info >> upload_private_school_contact_info
+        download_academic_year >> upload_academic_year
+        download_school_info_student_demographics >> upload_school_info_student_demographics
