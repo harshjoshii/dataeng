4e8eac84b6db5b7d354c3ea2a5cad22d38d0fcef
harshjoshi <harshjoshi211@gmail.com>
2020-09-30 16:40:48 -0400

    Export DOCKER_CLIENT_TIMEOUT and COMPOSE_HTTP_TIMEOUT variables in the start and stop services sell script

--- start-services.sh
+++ start-services.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-mkdir -p log
+
+echo -ne "Creating files and variables ..."
+mkdir -p log && \
 touch log/start-services.log && \
+export DOCKER_CLIENT_TIMEOUT=120 && \
+export COMPOSE_HTTP_TIMEOUT=120 && \
+echo  -e "\\rCreating files and variables ... \e[32mdone\e[0m" || \
+echo  -e "\\rCreating files and variables ... \e[31merror\e[0m"
+
+
 echo -ne "Starting services ..." && \
 docker-compose -f docker-compose-start-services.yml up -d &>> log/start-services.log && \
 echo  -e "\\rStarting services ... \e[32mdone\e[0m" || \
--- stop-services.sh
+++ stop-services.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-mkdir -p log
-touch log/stop-services.log && \
+echo -ne "Creating files and variables ..."
+mkdir -p log && \
+touch log/start-services.log && \
+export DOCKER_CLIENT_TIMEOUT=120 && \
+export COMPOSE_HTTP_TIMEOUT=120 && \
+echo  -e "\\rCreating files and variables ... \e[32mdone\e[0m" || \
+echo  -e "\\rCreating files and variables ... \e[31merror\e[0m"
+
 echo -ne "Stopping services ..." && \
 docker-compose -f docker-compose-start-services.yml down &>> log/stop-services.log && \
 echo  -e "\\rStopping services ... \e[32mdone\e[0m"
