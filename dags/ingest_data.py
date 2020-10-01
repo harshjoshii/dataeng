82268452af9717b955759a2dfe479dbe1ebbb519
harshjoshi <harshjoshi211@gmail.com>
2020-10-01 17:17:31 -0400

    Add tasks in ingest_data dag to convert xlsx file to csv using excel_lib

--- dags/ingest_data.py
+++ dags/ingest_data.py
@@ -4,6 +4,7 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from hdfs_lib import upload_to_hdfs
+from excel_lib import csv_from_excel
 
 default_args = {
     'owner' : 'airflow',
@@ -17,6 +18,8 @@ public_school_contact_info_dllink = "https://data.ontario.ca/dataset/fb3a7c18-90
 private_school_contact_info_dlink = "https://data.ontario.ca/dataset/7a049187-cf29-4ffe-9028-235b95c61fa3/resource/6545c5ec-a5ce-411c-8ad5-d66363da8891/download/private_schools_contact_information_september_2020_en.xlsx"
 academic_year_dlink = "https://files.ontario.ca/opendata/enrolment_by_school_1718_en_supp.xlsx"
 school_info_student_demographics_dlink = "https://data.ontario.ca/dataset/d85f68c5-fcb0-4b4d-aec5-3047db47dcd5/resource/602a5186-67f5-4faf-94f3-7c61ffc4719a/download/new_sif_data_table_2018_2019prelim_en_august.xlsx"
+shared_volume_path = "/shared_volume/ontario_school_data/"
+hdfs_path = "/dataeng/"
 
 with DAG('ingest_data',
         default_args=default_args,
@@ -25,21 +28,84 @@ with DAG('ingest_data',
         catchup=False) as dag:
 
         # Download files to a shared volume
-        download_school_board_school_authority_contact = BashOperator(task_id='download_school_board_school_authority_contact', bash_command='wget -O /shared-volume/ontario-school-data/' + school_board_school_authority_contact_dlink.split("/")[-1] + ' ' + school_board_school_authority_contact_dlink)
-        download_public_school_contact_info = BashOperator(task_id='download_public_school_contact_info', bash_command='wget -O /shared-volume/ontario-school-data/' + public_school_contact_info_dllink.split("/")[-1] + ' ' + public_school_contact_info_dllink)
-        download_private_school_contact_info = BashOperator(task_id='download_private_school_contact_info', bash_command='wget -O /shared-volume/ontario-school-data/' + private_school_contact_info_dlink.split("/")[-1] + ' ' + private_school_contact_info_dlink)
-        download_academic_year = BashOperator(task_id='download_academic_year', bash_command='wget -O /shared-volume/ontario-school-data/'+ academic_year_dlink.split("/")[-1] + ' ' + academic_year_dlink)
-        download_school_info_student_demographics = BashOperator(task_id='download_school_info_student_demographics', bash_command='wget -O /shared-volume/ontario-school-data/'+ school_info_student_demographics_dlink.split("/")[-1] + ' ' + school_info_student_demographics_dlink)
+        download_school_board_school_authority_contact = BashOperator(task_id='download_school_board_school_authority_contact', \
+                                                                        bash_command='wget -O ' + shared_volume_path + school_board_school_authority_contact_dlink.split("/")[-1] + ' ' + school_board_school_authority_contact_dlink)
+        download_public_school_contact_info = BashOperator(task_id='download_public_school_contact_info', \
+                                                            bash_command='wget -O ' + shared_volume_path + public_school_contact_info_dllink.split("/")[-1] + ' ' + public_school_contact_info_dllink)
+        download_private_school_contact_info = BashOperator(task_id='download_private_school_contact_info', \
+                                                            bash_command='wget -O ' + shared_volume_path + private_school_contact_info_dlink.split("/")[-1] + ' ' + private_school_contact_info_dlink)
+        download_academic_year = BashOperator(task_id='download_academic_year', \
+                                                bash_command='wget -O ' + shared_volume_path+ academic_year_dlink.split("/")[-1] + ' ' + academic_year_dlink)
+        download_school_info_student_demographics = BashOperator(task_id='download_school_info_student_demographics', \
+                                                                    bash_command='wget -O ' + shared_volume_path + school_info_student_demographics_dlink.split("/")[-1] + ' ' + school_info_student_demographics_dlink)
         
         # Upload files to HDFS
-        upload_school_board_school_authority_contact = PythonOperator(task_id='upload_school_board_school_authority_contact', python_callable=upload_to_hdfs, op_kwargs={'fileName': school_board_school_authority_contact_dlink.split("/")[-1]}, dag=dag)
-        upload_public_school_contact_info = PythonOperator(task_id='upload_public_school_contact_info', python_callable=upload_to_hdfs, op_kwargs={'fileName': public_school_contact_info_dllink.split("/")[-1]}, dag=dag)
-        upload_private_school_contact_info = PythonOperator(task_id='upload_private_school_contact_info', python_callable=upload_to_hdfs, op_kwargs={'fileName': private_school_contact_info_dlink.split("/")[-1]}, dag=dag)
-        upload_academic_year = PythonOperator(task_id='upload_academic_year_dlink', python_callable=upload_to_hdfs, op_kwargs={'fileName': academic_year_dlink.split("/")[-1]}, dag=dag)
-        upload_school_info_student_demographics = PythonOperator(task_id='upload_school_info_student_demographics', python_callable=upload_to_hdfs, op_kwargs={'fileName': school_info_student_demographics_dlink.split("/")[-1]}, dag=dag)
+        upload_school_board_school_authority_contact = PythonOperator(task_id='upload_school_board_school_authority_contact', \
+                                                                        python_callable=upload_to_hdfs, \
+                                                                        op_kwargs={'file_name': school_board_school_authority_contact_dlink.split("/")[-1], \
+                                                                                    'file_path': shared_volume_path, \
+                                                                                    'hdfs_path': hdfs_path}, \
+                                                                        dag=dag)
+        upload_public_school_contact_info = PythonOperator(task_id='upload_public_school_contact_info', \
+                                                            python_callable=upload_to_hdfs, \
+                                                            op_kwargs={'file_name': public_school_contact_info_dllink.split("/")[-1], \
+                                                                        'file_path': shared_volume_path, \
+                                                                        'hdfs_path': hdfs_path}, \
+                                                            dag=dag)
+        upload_private_school_contact_info = PythonOperator(task_id='upload_private_school_contact_info', \
+                                                            python_callable=upload_to_hdfs, \
+                                                            op_kwargs={'file_name': private_school_contact_info_dlink.split("/")[-1], \
+                                                                        'file_path': shared_volume_path, \
+                                                                        'hdfs_path': hdfs_path}, \
+                                                            dag=dag)
+        upload_academic_year = PythonOperator(task_id='upload_academic_year_dlink', \
+                                                python_callable=upload_to_hdfs, \
+                                                op_kwargs={'file_name': academic_year_dlink.split("/")[-1], \
+                                                                        'file_path': shared_volume_path, \
+                                                                        'hdfs_path': hdfs_path}, \
+                                                dag=dag)
+        upload_school_info_student_demographics = PythonOperator(task_id='upload_school_info_student_demographics', \
+                                                                    python_callable=upload_to_hdfs, \
+                                                                    op_kwargs={'file_name': school_info_student_demographics_dlink.split("/")[-1], \
+                                                                        'file_path': shared_volume_path, \
+                                                                        'hdfs_path': hdfs_path}, \
+                                                                    dag=dag)
 
-        download_school_board_school_authority_contact >> upload_school_board_school_authority_contact
-        download_public_school_contact_info >> upload_public_school_contact_info
-        download_private_school_contact_info >> upload_private_school_contact_info
-        download_academic_year >> upload_academic_year
-        download_school_info_student_demographics >> upload_school_info_student_demographics
+        # Convert xls file to csv
+        convert_school_board_school_authority_contact = PythonOperator(task_id='convert_school_board_school_authority_contact', \
+                                                                        python_callable=csv_from_excel, \
+                                                                        op_kwargs={'xlsx_file': shared_volume_path + school_board_school_authority_contact_dlink.split("/")[-1], \
+                                                                            'sheet_index' : 0, 'csv_file': shared_volume_path + school_board_school_authority_contact_dlink.split("/")[-1].split(".")[0]+".csv"}, \
+                                                                        dag=dag)
+        convert_public_school_contact_info = PythonOperator(task_id='convert_public_school_contact_info', \
+                                                                        python_callable=csv_from_excel, \
+                                                                        op_kwargs={'xlsx_file': shared_volume_path + public_school_contact_info_dllink.split("/")[-1], \
+                                                                            'sheet_index' : 0, 'csv_file': shared_volume_path + public_school_contact_info_dllink.split("/")[-1].split(".")[0]+".csv"}, \
+                                                                        dag=dag)                                                            
+        
+        convert_private_school_contact_info = PythonOperator(task_id='convert_private_school_contact_info', \
+                                                                        python_callable=csv_from_excel, \
+                                                                        op_kwargs={'xlsx_file': shared_volume_path + private_school_contact_info_dlink.split("/")[-1], \
+                                                                            'sheet_index' : 0, 'csv_file': shared_volume_path + private_school_contact_info_dlink.split("/")[-1].split(".")[0]+".csv"}, \
+                                                                        dag=dag) 
+        convert_private_school_contact_info_s1 = PythonOperator(task_id='convert_private_school_contact_info_s1', \
+                                                                        python_callable=csv_from_excel, \
+                                                                        op_kwargs={'xlsx_file': shared_volume_path + private_school_contact_info_dlink.split("/")[-1], \
+                                                                            'sheet_index' : 1, 'csv_file': shared_volume_path + private_school_contact_info_dlink.split("/")[-1].split(".")[0]+"_s1.csv"}, \
+                                                                        dag=dag) 
+        convert_academic_year = PythonOperator(task_id='convert_academic_year', \
+                                                                        python_callable=csv_from_excel, \
+                                                                        op_kwargs={'xlsx_file': shared_volume_path + academic_year_dlink.split("/")[-1], \
+                                                                            'sheet_index' : 0, 'csv_file': shared_volume_path + academic_year_dlink.split("/")[-1].split(".")[0]+".csv"}, \
+                                                                        dag=dag)
+        convert_school_info_student_demographics = PythonOperator(task_id='convert_school_info_student_demographics', \
+                                                                        python_callable=csv_from_excel, \
+                                                                        op_kwargs={'xlsx_file': shared_volume_path + school_info_student_demographics_dlink.split("/")[-1], \
+                                                                            'sheet_index' : 0, 'csv_file': shared_volume_path + school_info_student_demographics_dlink.split("/")[-1].split(".")[0]+".csv"}, \
+                                                                        dag=dag)
+        
+        download_school_board_school_authority_contact >> upload_school_board_school_authority_contact >> convert_school_board_school_authority_contact
+        download_public_school_contact_info >> upload_public_school_contact_info >> convert_public_school_contact_info
+        download_private_school_contact_info >> upload_private_school_contact_info >> convert_private_school_contact_info >> convert_private_school_contact_info_s1
+        download_academic_year >> upload_academic_year >> convert_academic_year
+        download_school_info_student_demographics >> upload_school_info_student_demographics >> convert_school_info_student_demographics
