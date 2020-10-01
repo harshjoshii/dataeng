30a89b516a4361d9b72fe1f259ce27b708da301b
harshjoshi <harshjoshi211@gmail.com>
2020-10-01 17:11:28 -0400

    Add arguments:file_path and hdfs_path to the method upload_to_hdfs to make it more dynamic

--- dags/hdfs_lib.py
+++ dags/hdfs_lib.py
@@ -3,8 +3,8 @@ from hdfs import InsecureClient
 
 client_hdfs  = InsecureClient('http://namenode:9870')
 
-def upload_to_hdfs(fileName):
-    client_hdfs.upload("/dataeng/", "/shared-volume/ontario-school-data/"+fileName, chunk_size=65536, cleanup=True)
+def upload_to_hdfs(file_name, file_path, hdfs_path):
+    client_hdfs.upload(hdfs_path, file_path+file_name, chunk_size=65536, cleanup=True)
     
 
 
